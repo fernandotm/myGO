5213098759ac2ccbaef4771c6a78704ce1a16905
Fernando Torres <ftorres@tmvalue.com>
2017-01-28 17:31:20 -0800

    correction

--- src/github.com/fernandotm/square.go
+++ src/github.com/fernandotm/square.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 )
 
-func Sqrt(x float64) float64 {
+func sqrt(x float64) float64 {
 	y := 0.000000
-	z := x/2
+	z := x / 2
 	i := 0
 	for i < 10 {
-		y = z - (z*z -x)/(2*z)
+		y = z - (z*z-x)/(2*z)
 		i++
 		z = y
 		fmt.Println(y)

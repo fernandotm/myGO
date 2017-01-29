9385be8a6d2ec6815bf08c1b8e67ce249a260041
Fernando Torres <ftorres@tmvalue.com>
2017-01-28 17:30:29 -0800

    tour lessons

---
+++ src/github.com/fernandotm/square.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+	y := 0.000000
+	z := x/2
+	i := 0
+	for i < 10 {
+		y = z - (z*z -x)/(2*z)
+		i++
+		z = y
+		fmt.Println(y)
+	}
+	return y
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
